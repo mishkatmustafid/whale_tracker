cdc27c4d785b30d2c858b8a391adf834c20f1950
Mishkat Mustafid <mishkatmustafid@gmail.com>
2022-12-15 10:30:52 +0600

    index.js updated

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const { ethers, Contract } = require("ethers")
 const player = require("play-sound")((opts = {}))
-
 const rpcURL = "https://cloudflare-eth.com"
 const provider = new ethers.providers.JsonRpcProvider(rpcURL)
-const contract_details = require("./contract_details.json")
 
+const contract_details = require("./contract_details.json")
 const CONTRACT_ADDRESS = contract_details["address"]
 const CONTRACT_ABI = contract_details["abi"]
-
 const contract = new Contract(CONTRACT_ADDRESS, CONTRACT_ABI, provider)
 
 // Note: USDC uses 6 decimal places
-const TRANSFER_THRESHOLD = 100000000000 // 100,000 USDC (in wei)
+const TRANSFER_THRESHOLD = 100000000000 // wei
 
 const playSound = () => {
   player.play("ding.mp3", function (err) {
@@ -20,13 +18,16 @@ const playSound = () => {
 }
 
 const main = async () => {
-  playSound()
+  // playSound()
   const name = await contract.name()
   console.log(
     `Whale tracker started!\nListening for large transfers on ${name}`
   )
 
   contract.on("Transfer", (from, to, amount, data) => {
+    // Note: not all ERC-20 tokens index `amount`
+    // Use this instead of Ethers.js query filters
+    // https://docs.ethers.io/v5/concepts/events/
     if (amount.toNumber() >= TRANSFER_THRESHOLD) {
       console.log(
         `New whale transfer for ${name}: https://etherscan.io/tx/${data.transactionHash}`
